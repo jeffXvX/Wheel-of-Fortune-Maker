9060d9535e176e73880d560c48fadeba179d6b8b
tme321 <tme321@gmail.com>
2019-02-07 17:21:57 -0500

    Cleanup after making it work

--- WoFCustomizer/src/app/features/home/instructions/instructions.component.ts
+++ WoFCustomizer/src/app/features/home/instructions/instructions.component.ts
@@ -12,15 +12,12 @@ export class InstructionsComponent implements OnInit {
   dataUrl: SafeResourceUrl;
   constructor(private sanitizer: DomSanitizer) {
     // just a quick and dirty fix for pulling in the instructions 
-    console.log('is prod?: ',environment.production)
     if(!environment.production) {
       this.dataUrl = this.sanitizer.bypassSecurityTrustResourceUrl('/assets/instructions/wof_instructions.html');
     }
     else {
       this.dataUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://jeffxvx.github.io/Wheel-of-Fortune-Maker/assets/instructions/wof_instructions.html');
     }
-
-    console.log('using url: ',this.dataUrl);
    }
 
   ngOnInit() {
